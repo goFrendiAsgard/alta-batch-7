eb061353f730a95d2e65a0e545982220e2951a00
Go Frendi Gunawan <gofrendiasgard@gmail.com>
2022-02-22 16:07:21 +0700

    perubahan dua

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Perubahan Anton")
+    fmt.Println("Perubahan Anton 2")
     fmt.Println("Hello world")
     fmt.Println("Perubahan Budi")
 }
